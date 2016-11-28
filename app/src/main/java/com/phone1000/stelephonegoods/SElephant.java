165b8b45f6eb8736a1e66bf2da06bfd1da342f59
my <1148432462@qq.com>
2016-11-28 10:17:11 +0800

    基本的框架构建

---
+++ app/src/main/java/com/phone1000/stelephonegoods/SElephant.java
@@ -0,0 +1,14 @@
+package com.phone1000.stelephonegoods;
+
+import android.app.Application;
+
+/**
+ * Created by my on 2016/11/28.
+ */
+public class SElephant extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
