e3c7347d7ce15c97b0feda100e46160cd6e23c4f
Administrator <1052379590@qq.com>
2016-11-28 14:30:31 +0800

    首页完成基本框架

--- app/src/main/java/com/phone1000/stelephonegoods/constant/ReadUrl.java
+++ app/src/main/java/com/phone1000/stelephonegoods/constant/ReadUrl.java
@@ -6,4 +6,5 @@ package com.phone1000.stelephonegoods.constant;
 public class ReadUrl {
     public static final String READ_URL="http://120.27.141.138:8080/dsp-manager-new/api/getArticles.json";
     public static final String HOMETITLEURL="http://api.xiaoxiangyoupin.com/v2/good/getPageViews.json";
+    public static final String HANDPICKURL="http://api.xiaoxiangyoupin.com/v2/good/goodGroups.json";
 }
