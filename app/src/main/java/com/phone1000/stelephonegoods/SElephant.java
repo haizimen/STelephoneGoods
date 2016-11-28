73692bd1404b3c549bd1c3e284816db1643e4fdc
Administrator <1052379590@qq.com>
2016-11-28 14:23:26 +0800

    首页完成基本框架

--- app/src/main/java/com/phone1000/stelephonegoods/SElephant.java
+++ app/src/main/java/com/phone1000/stelephonegoods/SElephant.java
@@ -2,6 +2,10 @@ package com.phone1000.stelephonegoods;
 
 import android.app.Application;
 
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import okhttp3.OkHttpClient;
+
 /**
  * Created by my on 2016/11/28.
  */
@@ -9,6 +13,7 @@ public class SElephant extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        OkHttpUtils.initClient(okHttpClient);
     }
 }
--- app/src/main/java/com/phone1000/stelephonegoods/constant/ReadUrl.java
+++ app/src/main/java/com/phone1000/stelephonegoods/constant/ReadUrl.java
@@ -5,4 +5,5 @@ package com.phone1000.stelephonegoods.constant;
  */
 public class ReadUrl {
     public static final String READ_URL="http://120.27.141.138:8080/dsp-manager-new/api/getArticles.json";
+    public static final String HOMETITLEURL="http://api.xiaoxiangyoupin.com/v2/good/getPageViews.json";
 }
