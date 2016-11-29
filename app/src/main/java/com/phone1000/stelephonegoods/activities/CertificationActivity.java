1f233c5a32af39c57c32377874708568bd925ef8
my <1148432462@qq.com>
2016-11-29 21:08:11 +0800

    我的页面的基本框架

--- app/src/main/java/com/phone1000/stelephonegoods/activities/CertificationActivity.java
+++ app/src/main/java/com/phone1000/stelephonegoods/activities/CertificationActivity.java
@@ -2,14 +2,54 @@ package com.phone1000.stelephonegoods.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.phone1000.stelephonegoods.R;
 
-public class CertificationActivity extends AppCompatActivity {
+public class CertificationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnVerify;
+    private EditText username;
+    private EditText userId;
+    private EditText userContactor;
+    private EditText bankCard;
+    private EditText bankName;
+    private EditText phoneNum;
+    private EditText identifyCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_certification);
+        initView();
+    }
+
+    private void initView() {
+        ImageView btnBack = (ImageView) findViewById(R.id.certification_back);
+        EditText username = (EditText) findViewById(R.id.certification_textview_username);
+        EditText userId = (EditText) findViewById(R.id.certification_textview_userid);
+        EditText userContactor = (EditText) findViewById(R.id.certification_textview_usercontractor);
+        EditText bankCard = (EditText) findViewById(R.id.certification_textview_bankcard);
+        EditText bankName = (EditText) findViewById(R.id.certification_textview_bank);
+        EditText phoneNum = (EditText) findViewById(R.id.certification_textview_phonenum);
+        EditText identifyCode = (EditText) findViewById(R.id.certification_textview_identifycode);
+
+        btnVerify = (Button) findViewById(R.id.certification_btn_verify);
+        Button sendCode = (Button) findViewById(R.id.certification_button_sendCode);
+        btnBack.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.certification_back:
+                finish();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/phone1000/stelephonegoods/constant/ConstantStr.java
@@ -0,0 +1,13 @@
+package com.phone1000.stelephonegoods.constant;
+
+/**
+ * Created by my on 2016/11/29.
+ */
+public class ConstantStr {
+    public static final String MINEORDER="我的订单";
+    public static final String WAITPAY="代付款";
+    public static final String WAITGOODS="待收货";
+    public static final String CASH="现金券";
+    public static final String FREE="免息券";
+
+}
