e7914995b31197bc65fbb309b33c486032be4b06
my <1148432462@qq.com>
2016-11-29 15:24:58 +0800

    我的页面的基本框架

---
+++ app/src/main/java/com/phone1000/stelephonegoods/activities/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.phone1000.stelephonegoods.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.phone1000.stelephonegoods.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
